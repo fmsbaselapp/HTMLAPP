6ec103451c60288bec21b775c4b4716f44ad0b41
Timon <fmsbaselapp@gmail.com>
2019-03-21 12:30:00 +0100

    aufräumen

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$fmsbasel_html = file_get_contents('https://display.edubs.ch/gl1');
-$fmsbasel_doc = new DOMDocument();
-$fmsbasel_doc->loadHTML($fmsbasel_html);
-
-
-
-echo $fmsbasel_html
-?>
