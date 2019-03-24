d628ba63a951be8895c8518cecc6d8b96fa1d496
FMSBaselApp <fmsbaselapp@gmail.com>
2019-03-24 23:01:41 +0100

    1

--- GrabberTest/scrap2.js
+++ GrabberTest/scrap2.js
@@ -5,6 +5,12 @@ request('http://display.edubs.ch/gl1', (error, response, html) => {
     if(!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
 
-      
+      $('.panel-heading').each((i,el) => {
+        const title = $(el).find('.text')
+        .text()
+        .find()
+
+      console.log(title);
+      });
     }
-})
+});
