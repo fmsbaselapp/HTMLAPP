d93701b617d01ef20315ad9783e0cabdc66550b9
FMSBaselApp <fmsbaselapp@gmail.com>
2019-03-24 23:39:45 +0100

    1

--- GrabberTest/scrap2.js
+++ GrabberTest/scrap2.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio')
 
-request('http://display.edubs.ch/gl1', (error, response, html) => {
+request('http://display.edubs.ch/gm1', (error, response, html) => {
     if(!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
 
