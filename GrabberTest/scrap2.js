76efc5bedb90c6983a59b3d9bf77e073b6cb6f5f
FMSBaselApp <fmsbaselapp@gmail.com>
2019-03-25 23:04:10 +0100

    1

--- GrabberTest/scrap2.js
+++ GrabberTest/scrap2.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const writeStream = fs.createWriteStream('post.csv');
 
 //Write Headers
-writeStream.write(`wochentag,ausfall,raum,alles \n`);
+writeStream.write(`wochentag,ausfall,raum \n`);
 
 request('http://display.edubs.ch/gm1', (error, response, html) => {
     if(!error && response.statusCode == 200) {
@@ -23,13 +23,9 @@ request('http://display.edubs.ch/gm1', (error, response, html) => {
           .find('div.panel-footer')
           .text()
           .replace(/\s\s+/g, ',');
-        const alles = $(el)
-          .find('div.panel.panel-default')
-          .text()
-          .replace(/\s\s+/g, ',');
 
       //Write Row To CSV
-      writeStream.write(`${wochentag}, ${ausfall}, ${raum}, ${alles} \n`);
+      writeStream.write(`${wochentag}, ${ausfall}, ${raum} \n`);
 
 
     });
