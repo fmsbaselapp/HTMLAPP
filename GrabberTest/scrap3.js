a0057b9b249ae5d9c04c1406540ccbe7a1458b37
FMSBaselApp <fmsbaselapp@gmail.com>
2019-03-25 23:02:18 +0100

    1

---
+++ GrabberTest/scrap3.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
+
+//Write Headers
+writeStream.write(`alles \n`);
+
+request('http://display.edubs.ch/gm1', (error, response, html) => {
+    if(!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      $('.container').each((i,el) => {
+        const alles = $(el)
+          .find('div.panel.panel-default')
+          .text()
+          .replace(/\s\s+/g, ',');
+
+      //Write Row To CSV
+      writeStream.write(`${alles} \n`);
+
+
+    });
+
+    console.log('Fertig...');
+  }
+});
