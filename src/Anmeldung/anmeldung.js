164630bb583a6afadbc873b0debc5195b4cf56e3
Timon <fmsbaselapp@gmail.com>
2019-04-27 01:25:24 +0200

    changes

--- src/Anmeldung/anmeldung.js
+++ src/Anmeldung/anmeldung.js
@@ -1,26 +1,30 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in.
-
+    // Nutzer ist angemeldet
+    /*
     document.getElementById("user_div").style.display = "block";
     document.getElementById("anmeldung-karte").style.display = "none";
-    document.getElementById("button").style.display = "none";
+    document.getElementById("button").style.display = "none";               //timon
+    */
 
     var user = firebase.auth().currentUser;
 
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Eigeloggt mit : " + email_id;
+      //document.getElementById("user_para").innerHTML = "Eigeloggt mit : " + email_id;     //timon
 
+      //weiterleitung test seite von timon ==========================================================
+      window.location.href = "https://classmateapp.online/benutzer.html"
+      //=============================================================================================
     }
 
   } else {
-    // No user is signed in.
+    //dies wird angezeigt, wenn niemand angemeldet ist. Standartseite
 
     document.getElementById("user_div").style.display = "none";
     document.getElementById("anmeldung-karte").style.display = "block";
-    document.getElementById("button").style.display = "block";
+    document.getElementById("button").style.display = "flex";
 
   }
 });
@@ -31,7 +35,7 @@ function login(){
   var userPass = document.getElementById("password_field").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-    // Handle Errors here.
+    // Hier wird der Error wiedergegeben
     var errorCode = error.code;
     var errorMessage = "Es ist ein Fehler aufgetreten: Deine Edubs-Mail oder dein Passwort stimmt nicht überein!";
 
