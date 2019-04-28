181485c3b76e9f28972b22f784406087e073d804
FMSBaselApp <fmsbaselapp@gmail.com>
2019-04-28 23:57:40 +0200

    1

--- src/Registrierung/registrierung.js
+++ src/Registrierung/registrierung.js
@@ -1,47 +1,47 @@
 firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-  
-      document.getElementById("user_div").style.display = "block";
-      document.getElementById("registrierung-karte").style.display = "none";
-  
-      var user = firebase.auth().currentUser;
-  
-      if(user != null){
-  
-        var email_id = user.email;
-        document.getElementById("user_para").innerHTML = "Du hast dich erfolgreich registriert: " + email_id;
-  
-      }
-  
-    } else {
-      // No user is signed in.
-  
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("registrierung-karte").style.display = "block";
-  
+  if (user) {
+    // Nutzer ist angemeldet
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("registrierung-karte").style.display = "none";
+    document.getElementById("button").style.display = "none";
+
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Eigeloggt mit : " + email_id;    
     }
+
+  } else {
+    //dies wird angezeigt, wenn niemand angemeldet ist. Standartseite
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("registrierung-karte").style.display = "block";
+    document.getElementById("button").style.display = "flex";
+
+  }
+});
+
+function create_user(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Hier wird der Error wiedergegeben
+    var errorCode = error.code;
+    var errorMessage = "Es ist ein Fehler aufgetreten: Deine Edubs-Mail oder dein Passwort stimmt nicht überein!";
+
+    window.alert(errorMessage);
+
+    // ...
   });
-  
-  function create_account(){
-  
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-    var userPassBes =document.getElementById("password_field_bestätigen").value;
-  
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
-    });
-  
-  }
-  
-  function logout(){
-    firebase.auth().signOut();
-  }
-  
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
