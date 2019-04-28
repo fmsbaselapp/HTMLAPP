32845d119f25602525d7b7ba0216399d7e549570
FMSBaselApp <fmsbaselapp@gmail.com>
2019-04-28 23:50:00 +0200

    1

--- src/Registrierung/registrierung.js
+++ src/Registrierung/registrierung.js
@@ -3,14 +3,14 @@ firebase.auth().onAuthStateChanged(function(user) {
       // User is signed in.
   
       document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
+      document.getElementById("registrierung-karte").style.display = "none";
   
       var user = firebase.auth().currentUser;
   
       if(user != null){
   
         var email_id = user.email;
-        document.getElementById("user_para").innerHTML = "Du bist angemeldet: " + email_id;
+        document.getElementById("user_para").innerHTML = "Du hast dich erfolgreich registriert: " + email_id;
   
       }
   
@@ -18,17 +18,18 @@ firebase.auth().onAuthStateChanged(function(user) {
       // No user is signed in.
   
       document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
+      document.getElementById("registrierung-karte").style.display = "block";
   
     }
   });
   
-  function login(){
+  function create_account(){
   
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
+    var userPassBes =document.getElementById("password_field_bestätigen").value;
   
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
