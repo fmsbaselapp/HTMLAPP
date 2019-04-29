3d884fe521c32b318c2610c9ac49d0656c43bfba
FMSBaselApp <fmsbaselapp@gmail.com>
2019-04-30 00:59:38 +0200

    1

--- src/Passwort/passwort.js
+++ src/Passwort/passwort.js
@@ -4,12 +4,21 @@ function passwort_zurücksetzen(){
 
 auth.sendPasswordResetEmail(emailAddress).then(function() {
   // Email sent.
+
+  document.getElementById("user_div").style.display = "none";
+  document.getElementById("anmeldung-karte").style.display = "block";
+  document.getElementById("button").style.display = "flex";
+
+
 }).catch(function(error) {
   // An error happened.
   var errorCode = error.code;
   var errorMessage = "Es ist ein Fehler aufgetreten: Deine Edubs-Mail wurde nicht gefunden.";
 
+  document.getElementById("user_div").style.display = "block";
+  document.getElementById("anmeldung-karte").style.display = "none";
+  document.getElementById("button").style.display = "none";
+
   window.alert(errorMessage);
 });
 }
-
